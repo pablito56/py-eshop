35bb38605c9f9fe7eda2581e394eef1ec4dee9bf
pablito56 <pablito56@gmail.com>
2012-11-26 20:21:03 +0100

    Middleware to process API exceptions

--- basesite/basesite/settings.py
+++ basesite/basesite/settings.py
@@ -112,6 +112,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'eshop.middleware.EshopMiddleware'
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
---
+++ basesite/eshop/middleware.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+u'''
+Created on Nov 19, 2012
+
+@author: pev
+@author: jacobcr
+'''
+# Django imports
+from django.http import HttpResponse
+# Rest framework imports
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+# Pymongo imports
+from pymongo.errors import DuplicateKeyError
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders it's content into JSON.
+    """
+    renderer = JSONRenderer()
+
+    def __init__(self, data, **kwargs):
+        content = self.renderer.render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+class EshopMiddleware(object):
+    '''Middleware class to handle internal exceptions
+    '''
+    def process_exception(self, request, exception):
+        # Check for MongoDB unique indexes exception 
+        if isinstance(exception, DuplicateKeyError):
+            msg = "Trying to create a resource which already exists"
+            return JSONResponse({"msg": msg}, status=status.HTTP_409_CONFLICT)
+        # Check if failed accesing a key of a dict
+        elif isinstance(exception, KeyError):
+            msg = "Trying to retrieve a wrong document key '{0'}".format(KeyError)
+            return JSONResponse({"msg": msg}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # Otherwise
+        import traceback
+        print "Unexpected e-shop exception caught.", traceback.format_exc()
+        msg = "Unexpected error ({0}): {1}".format(exception.__class__.__name__, exception)
+        return JSONResponse({"msg": msg}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
